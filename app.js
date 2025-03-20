c5e9fdfb9271a76e98ede1d7416a1baea54a9f99
Sai-778 <sai423453@gmail.com>
2025-03-21 01:10:03 +0530

    save to libarary and adding it to localstorage

--- app.js
+++ app.js
@@ -252,12 +252,17 @@ const submitSave =document.querySelector(".submit-save");
 const closeSave =document.querySelector(".close-save");
 const saveContainer =document.querySelector(".save-container");
 const saveInput =document.querySelector(".save-container input");
-
+const libraryContainer =document.querySelector(".library-container");
+const libraryBtn =document.querySelector(".library");
+const closeLibraryBtn =document.querySelector(".close-library");
 
 //event Listeners
 saveBtn.addEventListener('click',openPalette);
 closeSave.addEventListener('click',closePalette);
 submitSave.addEventListener('click',savePalette);
+libraryBtn.addEventListener('click',openLibrary);
+closeLibraryBtn.addEventListener('click',closeLibrary);
+
 
 
 function openPalette(e){
@@ -288,6 +293,31 @@ function savePalette(e){
   //save to local storage
   savetoLocal(paletteObj);
   saveInput.value="";
+
+  //generate the palette for  library
+
+  const palette = document.createElement("div");
+  palette.classList.add("custom-palette");
+  const title =document.createElement("h4");
+  title.innerText = paletteObj.name;
+  const preview = document.createElement("div");
+  preview.classList.add("small-preview");
+  paletteObj.colors.forEach(smallColor => {
+      const smallDiv = document.createElement("div");
+      smallDiv.style.backgroundColor = smallColor;
+      preview.appendChild(smallDiv);
+  });
+  const paletteBtn= document.createElement("button");
+  paletteBtn.classList.add('pick-palette-btn');
+  paletteBtn.classList.add(paletteObj.nr);
+  paletteBtn.innerText ='Select';
+
+  //append to library
+palette.appendChild(title);
+palette.appendChild(preview);
+palette.appendChild(paletteBtn);
+libraryContainer.children[0].appendChild(palette);
+
 }
 
 function savetoLocal(paletteObj){
@@ -302,4 +332,15 @@ function savetoLocal(paletteObj){
 
 }
 
+function openLibrary(){
+  const popup = libraryContainer.children[0];
+  libraryContainer.classList.add("active");
+  popup.classList.add("active");
+}
+
+function closeLibrary(){
+  const popup = libraryContainer.children[0];
+  libraryContainer.classList.remove("active");
+  popup.classList.remove("active");
+}
 randomColors();
